140600a514595151ce7fed1ad926b4ea863ab08e
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-09-19 09:22:08 +0700

    u

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -85,6 +85,6 @@ app.get("/delete/:id", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(5000, () => {
+    console.log('Listening on port 5000');
 });
