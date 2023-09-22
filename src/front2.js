ea4b9f6e0b999690d66f2427526d19ef5d2e6f4f
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-09-22 17:26:09 +0700

    u

--- src/front2.js
+++ src/front2.js
@@ -51,7 +51,7 @@ app.post("/create1", async (req, res) => {
     try{
         const data = {name: req.body.name};
         await axios.post(base_url + '/rooms', data);
-        res.redirect('/');
+        res.redirect('/rooms');
     }catch(err){
         console.error(err);
         res.status(500).send("Error");
@@ -73,7 +73,7 @@ app.post("/update1/:id", async (req, res) => {
     try{
         const data = {name: req.body.name};
         await axios.put(base_url + '/rooms/' + req.params.id, data);
-        res.redirect('/');
+        res.redirect('/rooms');
     } catch(err){
         console.error(err);
         res.status(500).send("Error");
@@ -83,7 +83,145 @@ app.post("/update1/:id", async (req, res) => {
 app.get("/delete1/:id", async (req, res) => {
     try{
         await axios.delete(base_url + '/rooms/' + req.params.id);
-        res.redirect('/');
+        res.redirect('/rooms');
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+//<!-------------------------------------------------------------!>//
+
+app.get('/people', async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/people');
+        res.render('people', {people: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get('/person/:id', async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/people/' + req.params.id);
+        res.render('person', {person: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/create2", (req, res) => {
+    res.render("create2");
+});
+
+app.post("/create2", async (req, res) => {
+    try{
+        const data = {name: req.body.name};
+        await axios.post(base_url + '/people', data);
+        res.redirect('/people');
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/update2/:id", async (req, res) => {
+    try{
+        const response = await axios.get(
+        base_url + '/people/' + req.params.id);
+        res.render("update2", {person: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.post("/update2/:id", async (req, res) => {
+    try{
+        const data = {name: req.body.name};
+        await axios.put(base_url + '/people/' + req.params.id, data);
+        res.redirect('/people');
+    } catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/delete2/:id", async (req, res) => {
+    try{
+        await axios.delete(base_url + '/people/' + req.params.id);
+        res.redirect('/people');
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+//<!-------------------------------------------------------------!>//
+
+app.get('/renting', async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/renting');
+        res.render('renting', {renting: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get('/rent/:id', async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/renting/' + req.params.id);
+        res.render('rent', {rent: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/create3", (req, res) => {
+    res.render("create3");
+});
+
+app.post("/create3", async (req, res) => {
+    try{
+        const data = {r_id: req.body.r_id, p_id: req.body.p_id};
+        await axios.post(base_url + '/renting', data);
+        res.redirect('/renting');
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/update3/:id", async (req, res) => {
+    try{
+        const response = await axios.get(
+        base_url + '/renting/' + req.params.id);
+        res.render("update3", {rent: response.data});
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.post("/update3/:id", async (req, res) => {
+    try{
+        const data = {r_id: req.body.r_id, p_id: req.body.p_id};
+        await axios.put(base_url + '/renting/' + req.params.id, data);
+        res.redirect('/renting');
+    } catch(err){
+        console.error(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.get("/delete3/:id", async (req, res) => {
+    try{
+        await axios.delete(base_url + '/renting/' + req.params.id);
+        res.redirect('/renting');
     }catch(err){
         console.error(err);
         res.status(500).send("Error");
