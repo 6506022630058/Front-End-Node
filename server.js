7d22639a766614fddb050be8b83246cb42b208de
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-09-26 10:27:17 +0700

    u

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const path = require('path');
 //1
 
 // const base_url = 'http://localhost:3000';
-// const base_url = 'http://node50125-jsapp.proen.app.ruk-com.cloud';
-const base_url = 'http://node50139-jsapp.proen.app.ruk-com.cloud';
+const base_url = 'http://node50125-jsapp.proen.app.ruk-com.cloud';
+// const base_url = 'http://node50139-jsapp.proen.app.ruk-com.cloud';
 
 
 app.set("views",path.join(__dirname,"/public/views"));
